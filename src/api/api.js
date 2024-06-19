5ca17698a28c6a135278b1e1c1ce8adb57e7ed1e
kushel-reddy <kushel1422@gmail.com>
2024-06-19 09:57:05 +0530

    fix: utc conversion

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+const moment = require("moment")
 
-const API_URL = 'http://ec2-13-127-87-100.ap-south-1.compute.amazonaws.com:3000';
-// const API_URL = 'http://localhost:3000';
+// const API_URL = 'http://ec2-13-127-87-100.ap-south-1.compute.amazonaws.com:3000';
+const API_URL = 'http://localhost:3000';
 
 
 export const fetchMetricsWithRange = async (start, end, selectedMetric) => {
@@ -9,10 +10,14 @@ export const fetchMetricsWithRange = async (start, end, selectedMetric) => {
   const metrics = selectedMetric.map(metric => (
     metric.value
   ));
+
+  const startUTC = moment(start).utc().format();
+  const endUTC = moment(end).utc().format();
+
     try {
       const response = await axios.post(`${API_URL}/metrics`, {
-        start,
-        end,
+        startUTC,
+        endUTC,
         metrics
       });
       return response.data;
